5524110c7cb42d50344bb1f0747497df58762248
David Changea <14ir155@gmail.com>
2024-05-03 23:20:02 +0200

    mise en place de reduc

---
+++ frontend/src/apiConfig.js
@@ -0,0 +1,5 @@
+// Importation de la variable databaseUrl à partir des variables d'environnement de Vite
+const apiUrl = import.meta.env.VITE_API_URL;
+
+// Exportation de la variable databaseUrl pour la rendre accessible depuis d'autres fichiers
+export { apiUrl };
